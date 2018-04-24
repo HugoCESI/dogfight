5b038092c57f50956b7f31518bfd527a582ed60c
Thomas Baudru <thomas.baudru@viacesi.fr>
2018-04-24 10:25:35 +0200

    correction IDogFightMobile

--- src/fr/exia/dogfight/model/IDogFightModel.java
+++ src/fr/exia/dogfight/model/IDogFightModel.java
@@ -1,8 +1,13 @@
 package fr.exia.dogfight.model;
 
+import java.util.ArrayList;
+
 public interface IDogFightModel {
-	public void IArea();
-	public void IMobile();
-	public void Dimension();
-	public void Direction();
+	public IArea getArea();
+	public void buildArea(IMobile Mobile);
+	public void removeMobile(IMobile Mobile);
+	public ArrayList<IMobile> getMobiles();
+	public IMobile getMobileByPlayer(int player);
+	public void setMobilesHavesMoved();
+	
 }
