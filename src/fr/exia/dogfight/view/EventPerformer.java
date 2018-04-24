ec6a720694caca80955d169d30e79ce2d0acd8c2
Nelson <nelson.deren@viacesi.com>
2018-04-24 23:14:27 +0200

    essaie final

--- src/fr/exia/dogfight/view/EventPerformer.java
+++ src/fr/exia/dogfight/view/EventPerformer.java
@@ -3,6 +3,7 @@ package fr.exia.dogfight.view;
 import java.awt.event.KeyEvent;
 
 import fr.exia.dogfight.controller.IOrderPerformer;
+import fr.exia.dogfight.controller.Order;
 import fr.exia.dogfight.controller.UserOrder;
 import fr.exia.gameframe.IEventPerformer;
 
@@ -23,19 +24,115 @@ public class EventPerformer implements IEventPerformer {
 	
 	private UserOrder keyCodeToUserOrder(int keyCode) {
 		
+		int player = 0;
+		Order order = null;
+		
 		switch (keyCode) {
-		case :
-			return new UserOrder(1, "UP");
+		case KeyEvent.KEY_PRESSED:
+			switch(keyCode) {
+	        case KeyEvent.VK_RIGHT :
+	            order = Order.RIGHT;
+	            player = 1;
+	            break;
+	
+	        case KeyEvent.VK_LEFT:
+	            order = Order.LEFT;
+	            player = 1;
+	            break;
+	
+	        case KeyEvent.VK_DOWN:
+	            order = Order.DOWN;
+	            player = 1;
+	            break;
+	
+	        case KeyEvent.VK_UP:
+	            order = Order.UP;
+	            player = 1;
+	            break;
+	
+	        case KeyEvent.VK_Z:
+	            order = Order.UP;
+	            player = 2;
+	            break;
+	
+	        case KeyEvent.VK_D:
+	            order = Order.RIGHT;
+	            player = 2;
+	            break;
+	
+	        case KeyEvent.VK_Q:
+	            order = Order.LEFT;
+	            player = 2;
+	            break;
+	
+	        case KeyEvent.VK_S:
+	            order = Order.DOWN;
+	            player = 2;
+	            break;
+		
+	        default:
+	            order = Order.NOP;
+	        }
 			break;
 			
-		case 2:
-			return new UserOrder(2, "UP");
-			break;	
+		case KeyEvent.KEY_RELEASED:
 			
-		default:
+			order = Order.NOP;
+			
+			switch(keyCode) {
+	        case KeyEvent.VK_RIGHT :
+	            player = 1;
+	            break;
+	
+	        case KeyEvent.VK_LEFT:
+	            player = 1;
+	            break;
+	
+	        case KeyEvent.VK_DOWN:
+	            player = 1;
+	            break;
+	
+	        case KeyEvent.VK_UP:
+	            player = 1;
+	            break;
+	
+	        case KeyEvent.VK_Z:
+	            player = 2;
+	            break;
+	
+	        case KeyEvent.VK_D:
+	            player = 2;
+	            break;
+	
+	        case KeyEvent.VK_Q:
+	            player = 2;
+	            break;
+	
+	        case KeyEvent.VK_S:
+	            player = 2;
+	            break;
+	
+	        default:
+	            order = Order.NOP;
+	        }
 			break;
+		
+		case KeyEvent.KEY_TYPED:
+			switch (keyCode) {
+			case KeyEvent.VK_0:
+				order = Order.SHOOT;
+				player = 1;
+				break;
+				
+			case KeyEvent.VK_SPACE:
+				order = Order.SHOOT;
+				player = 2;
+				break;
+				
+			}
 		}
-		;
+		
+        return new UserOrder(player , order);
 	}
 	
 }
