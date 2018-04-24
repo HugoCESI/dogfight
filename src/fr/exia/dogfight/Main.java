b2e9c7c1ef8c8c63c41791fbf477fc28b2e8dc34
HugoCESI <hugo.fernandez@viacesi.fr>
2018-04-24 14:08:22 +0200

    Ajout main

--- src/fr/exia/dogfight/Main.java
+++ src/fr/exia/dogfight/Main.java
@@ -1,10 +1,18 @@
 package fr.exia.dogfight;
 
+import fr.exia.dogfight.controller.DogFightController;
+import fr.exia.dogfight.model.DogFightModel;
+import fr.exia.dogfight.view.DogFightView;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("CCC");
+		final DogFightModel dogfightModel = new DogFightModel();
+		final DogFightController dogfightController = new DogFightController(dogfightModel);
+		final DogFightView dogfightView = new DogFightView(dogfightController, dogfightModel, dogfightModel);
+		dogfightController.setViewSystem(dogfightView);
+		dogfightController.play();
 	}
 
 }
